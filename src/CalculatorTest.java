2c6c30d58073d5a66b2d28b0218303c8ca2d28b9
Prakash K <kprakash19940625@gmail.com>
2016-01-07 17:24:28 +0530

    Create CalculatorTest.java

---
+++ src/CalculatorTest.java
@@ -0,0 +1,27 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+
+public class CalculatorTest {
+
+        Calculator calculator;
+  @Before
+  public void init(){
+     calculator = new Calculator();
+  }
+  @Test
+  public void test() {
+    int sum = calculator.evaluate("1+2+3");
+    assertEquals(6, sum);
+  }
+  @Test
+  public void test1() {
+    int mine = calculator.evaluate("4+8+8");
+    assertEquals(20,mine);
+  }
+  @Test
+  public void test2() {
+    int mine = calculator.evaluate("2+7+4");
+    assertEquals(13,mine);
+  }
+}
