1ed9bbecf624477c2ed9ed8e9aa0119039f7e633
Prakash K <kprakash19940625@gmail.com>
2016-01-07 17:21:55 +0530

    Update TestRunner.java

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -4,10 +4,13 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
    public static void main(String[] args) {
-      Result result = JUnitCore.runClasses(TestJunit.class);
-      for (Failure failure : result.getFailures()) {
-         System.out.println(failure.toString());
-      }
-      System.out.println(result.wasSuccessful());
+      Result result = JUnitCore.runClasses(CalculatorTest.class);
+      int total_no = result.getRunCount();
+      int failure_no = result.getFailureCount();
+      int success_no = total_no - failure_no;
+      System.out.println("Total \t Success \t Failure");
+      System.out.print(total_no+"\t");
+      System.out.print(success_no+"\t");
+      System.out.println("\t"+failure_no);
    }
 }
