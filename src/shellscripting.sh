70470ed8eaf37cb893fbeef3fdaebdee2162dd4b
Prakash K <kprakash19940625@gmail.com>
2016-01-07 17:27:33 +0530

    Update shellscripting.sh

--- src/shellscripting.sh
+++ src/shellscripting.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export JUNIT_HOME=/usr/local/JUNIT
 export CLASSPATH=$CLASSPATH:$JUNIT_HOME/junit-4.10.jar:.
-javac TestJunit.java
+javac Calculator.java
+javac CalculatorTest.java
 javac TestRunner.java
 java TestRunner
