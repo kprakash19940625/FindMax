47f0b3d8063007c0d6980c395094c2f971319f0d
Prakash K <kprakash19940625@gmail.com>
2016-01-07 18:42:52 +0530

    Update TestRunner.java

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -8,8 +8,8 @@ public class TestRunner {
       int total_no = result.getRunCount();
       int failure_no = result.getFailureCount();
       int success_no = total_no - failure_no;
-      System.out.println("#########################################");
-      for(int i=0;i<20;i++)
+      System.out.println("#################################################################");
+      for(int i=0;i<10;i++)
          System.out.println("");
       System.out.println("Total \t Success \t Failure");
       System.out.print(total_no+"\t");
@@ -19,6 +19,6 @@ public class TestRunner {
       System.out.println(result.getFailures());
       for(int i=0;i<10;i++)
          System.out.println("");
-      System.out.println("#########################################");
+      System.out.println("################################################################");
    }
 }
